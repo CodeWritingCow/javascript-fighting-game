dd9d4f462eafd54ca887bdc166efc2b101e11304
Gary Pang <gary.pang@gmail.com>
2022-12-09 18:27:28 -0500

    Add logic for increasing velocity to Sprite class

--- index.js
+++ index.js
@@ -6,20 +6,29 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2;
+
 class Sprite {
     constructor({ position, velocity }) {
         this.position = position;
         this.velocity = velocity;
+        this.height = 150;
     }
 
     draw() {
         c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 50, 150);
+        c.fillRect(this.position.x, this.position.y, 50, this.height);
     }
 
     update() {
         this.draw();
-        this.position.y += 10;
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0;
+        } else {
+            this.velocity.y += gravity;
+        }
     }
 }
 
