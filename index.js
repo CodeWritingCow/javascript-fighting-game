7eff2ff3fe6da57e058867155d50636b37ecebd8
Gary Pang <gary.pang@gmail.com>
2022-12-09 16:52:24 -0500

    Add velocity property to Sprite class
    Add velocity property. Create function animate.

--- index.js
+++ index.js
@@ -7,28 +7,48 @@ canvas.height = 576;
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
-  constructor(position) {
-    this.position = position;
-  }
+    constructor({ position, velocity }) {
+        this.position = position;
+        this.velocity = velocity;
+    }
 
-  draw() {
-    c.fillStyle = 'red';
-    c.fillRect(this.position.x, this.position.y, 50, 150);
-  }
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, 50, 150);
+    }
 }
 
 const player = new Sprite({
-  x: 0,
-  y: 0
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
 });
 
 player.draw();
 
 const enemy = new Sprite({
-  x: 400,
-  y: 100
+  position: {
+      x: 400,
+      y: 100
+  },
+  velocity: {
+      x: 0,
+      y: 0
+  }
 });
 
 enemy.draw();
 
-console.log(player);
+// console.log(player);
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    // console.log('moo');
+}
+
+animate();
