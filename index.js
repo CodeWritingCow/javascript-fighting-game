8b8c8c28b821333c61a97538884a7d18b3b129fd
Gary Pang <gary.pang@gmail.com>
2022-12-09 19:05:35 -0500

    Fix bug with game not reacting to last key pressed
    Move logic for changing player velocity from event listeners
    to animate().

--- index.js
+++ index.js
@@ -55,12 +55,31 @@ const enemy = new Sprite({
     }
 });
 
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+};
+
+let lastKey;
+
 function animate() {
     window.requestAnimationFrame(animate);
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
     enemy.update();
+
+    player.velocity.x = 0;
+
+    if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -1;
+    } else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 1;
+    }
 }
 
 animate();
@@ -68,13 +87,15 @@ animate();
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'd':
-            player.velocity.x = 1;
+            keys.d.pressed = true;
+            lastKey = 'd';
             break;
         case 'a':
-            player.velocity.x = -1;
+            keys.a.pressed = true;
+            lastKey = 'a';
             break;
-        default:
-            break;
+        // default:
+        //     break;
     }
     console.log(event.key);
 });
@@ -82,13 +103,15 @@ window.addEventListener('keydown', (event) => {
 window.addEventListener('keyup', (event) => {
     switch (event.key) {
         case 'd':
-            player.velocity.x = 0;
+            keys.d.pressed = false;
             break;
         case 'a':
-            player.velocity.x = 0;
+            keys.a.pressed = false;
             break;
-        default:
-            break;
+        // default:
+        //     break;
     }
     console.log(event.key);
 });
+
+// https://www.youtube.com/watch?v=vyqbNFMDRGQ&t=1275s
