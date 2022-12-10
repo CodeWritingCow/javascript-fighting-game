28d12bd48c9c00b6165ede72585614bf4c48123c
Gary Pang <gary.pang@gmail.com>
2022-12-10 16:06:35 -0500

    Refactor code into function determineWinner
    Stop timer when either player wins, or both tie.

--- index.js
+++ index.js
@@ -126,25 +126,30 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     );
 }
 
+function determineWinner({ player, enemy, timerId }) {
+    clearTimeout(timerId);
+    document.querySelector('#displayText').style.display = 'flex';
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie';
+    } else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins';
+    } else if (enemy.health > player.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins';
+    }
+}
+
 let timer = 60;
+let timerId;
 
 function decreaseTimer() {
     if (timer > 0) {
-        setTimeout(decreaseTimer, 1000);
+        timerId = setTimeout(decreaseTimer, 1000);
         timer--;
         document.querySelector('#timer').innerHTML = timer;
     }
 
     if (timer === 0) {
-        document.querySelector('#displayText').style.display = 'flex';
-
-        if (player.health === enemy.health) {
-            document.querySelector('#displayText').innerHTML = 'Tie';
-        } else if (player.health > enemy.health) {
-            document.querySelector('#displayText').innerHTML = 'Player 1 Wins';
-        } else if (enemy.health > player.health) {
-            document.querySelector('#displayText').innerHTML = 'Player 2 Wins';
-        }
+        determineWinner({ player, enemy, timerId });
     }
 }
 
@@ -196,7 +201,7 @@ function animate() {
 
     // End game based on health
     if (enemy.health <= 0 || player.health <= 0) {
-      // TODO: Write code for end game
+        determineWinner({ player, enemy, timerId });
     }
 }
 
