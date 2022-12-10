43307f9a0d704bcf6131cfd7e5f5824c3481a228
Gary Pang <gary.pang@gmail.com>
2022-12-09 23:28:17 -0500

    Add Sprite.attackBox with collision detection
    Add attackBox property with collision detection.

--- index.js
+++ index.js
@@ -9,16 +9,32 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = 0.7;
 
 class Sprite {
-    constructor({ position, velocity }) {
+    constructor({ position, velocity, color = 'red' }) {
         this.position = position;
         this.velocity = velocity;
+        this.width = 50;
         this.height = 150;
         this.lastKey;
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50
+        };
+        this.color = color;
     }
 
     draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 50, this.height);
+        c.fillStyle = this.color;
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+        // attack box
+        c.fillStyle = 'green';
+        c.fillRect(
+            this.attackBox.position.x,
+            this.attackBox.position.y,
+            this.attackBox.width,
+            this.attackBox.height
+        );
     }
 
     update() {
@@ -53,7 +69,8 @@ const enemy = new Sprite({
     velocity: {
         x: 0,
         y: 0
-    }
+    },
+    color: 'blue'
 });
 
 const keys = {
@@ -93,6 +110,18 @@ function animate() {
         enemy.velocity.x = -5;
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
         enemy.velocity.x = 5;
+    }
+
+    // detect for collision
+    if (
+        player.attackBox.position.x + player.attackBox.width >=
+            enemy.position.x &&
+        player.attackBox.position.x <= enemy.position.x + enemy.width &&
+        player.attackBox.position.y + player.attackBox.height >=
+            enemy.position.y &&
+        player.attackBox.position.y <= enemy.position.y + enemy.height
+    ) {
+        console.log('go');
     }
 }
 
