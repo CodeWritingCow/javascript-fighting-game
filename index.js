8da39ff657b7cf9fede64d85df2f6ebda2d05e10
Gary Pang <gary.pang@gmail.com>
2022-12-10 00:43:06 -0500

    Add health bar logic and styling
    Add logic for reducing player and enemy health.

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
         };
         this.color = color;
         this.isAttacking;
+        this.health = 100;
     }
 
     draw() {
@@ -155,7 +156,8 @@ function animate() {
         player.isAttacking
     ) {
         player.isAttacking = false;
-        console.log('go');
+        enemy.health -= 20;
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%';
     }
 
     if (
@@ -163,7 +165,8 @@ function animate() {
         enemy.isAttacking
     ) {
         enemy.isAttacking = false;
-        console.log('enemy attack successful');
+        player.health -= 20;
+        document.querySelector('#playerHealth').style.width = player.health + '%';
     }
 }
 
