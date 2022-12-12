5c3bf7e1c6221e98e18d000fe50a6e619e614935
Gary Pang <gary.pang@gmail.com>
2022-12-12 15:47:50 -0500

    Add white overlay to background for contrast

--- index.js
+++ index.js
@@ -170,6 +170,8 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height);
     background.update();
     shop.update();
+    c.fillStyle = 'rgba(255, 255, 255, 0.15)';
+    c.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
     enemy.update();
 
